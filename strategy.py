d731f7069b58441ee258fd8f0fa2c83c72881885
nathansizer <52630545+nathansizer@users.noreply.github.com>
2025-09-21 16:45:26 +0100

    Create the strategy class

---
+++ strategy.py
@@ -0,0 +1,17 @@
+"""
+My strategy class
+"""
+
+from tyres import tyreSets
+
+class Strategy:
+
+    #initialiser
+    def __init__(self, pitLaps, tyreOrder):
+        self.pitLaps = pitLaps
+        self.tyreOrder = tyreOrder
+
+
+    #create a text representation of this strategy
+    def __repr__(self):
+        return f"Strategy(pits={self.pitLaps}, tyres={[t.name for t in self.tyreOrder]})"
