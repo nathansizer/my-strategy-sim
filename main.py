c051e9fe471d0f2503b8045d5c99c0723c13a448
nathansizer <52630545+nathansizer@users.noreply.github.com>
2025-09-21 17:21:44 +0100

    create main class

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+The main file of the project.
+
+Everything is run from here, simulations are compared and the best is printed.
+"""
+
+from race import Race
+from strategy import Strategy
+
+
+#define a race, here using the same parameters set out in the readme file
+race = Race(52, 92.0, 25.0)
+
+#define some strategies
+strategies = [
+
+    Strategy([25], ["medium", "hard"]),
+    Strategy([15, 30], ["soft", "soft", "hard"]),
+    Strategy([18, 34], ["soft", "medium", "medium"])
+
+]
+
+#work out race time results
+results = []
+
+for s in strategies:
+        
+    time, laps = race.simStrategy(s)
+    results.append({"strategy": s, "time": time, "lapTimes": laps})
+
+bestStrategy = min(results, key = lambda x: x["time"])
+
+print("Fastest Strategy: " bestStrategy["strategy"])
+print("Total Race Time: ", round(best["time"], 2), " seconds")
