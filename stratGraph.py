2322041bdb6e71ffe91efe4f86b4460f70281278
nathansizer <52630545+nathansizer@users.noreply.github.com>
2025-09-22 18:52:58 +0100

    Add some more styling to the graph, just to make it look a bit more professional

--- stratGraph.py
+++ stratGraph.py
@@ -7,6 +7,19 @@ import numpy as np
 #plot the strategies on a graph
 def plotStrategies(strats):
 
+    plt.style.use("seaborn-v0_8-darkgrid")
+
+    plt.rcParams.update({
+        "figure.figsize": (12, 6),
+        "axes.titlesize": 16,
+        "axes.labelsize": 14,
+        "legend.frameon": True,
+        "legend.loc": "best",
+        "lines.linewidth": 2.5,
+        "lines.markersize": 7,
+        "axes.facecolor": "#f9f9f9",
+    })
+
     plt.figure(figsize = (10,6))
 
     totalTimes = [np.cumsum(s["lapTimes"]) for s in strats]
