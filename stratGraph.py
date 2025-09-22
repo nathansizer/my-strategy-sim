ec6667fcedc0a8417e93d5e5ad34cda6b36c2a8e
nathansizer <52630545+nathansizer@users.noreply.github.com>
2025-09-22 18:05:49 +0100

    Implement code to show the strategies on a graph

--- stratGraph.py
+++ stratGraph.py
@@ -1,3 +1,21 @@
 """
 Strategy Graph and plotting class
 """
+import matplotlib.pyplot as plt
+
+#plot the strategies on a graph
+def plotStrategies(strats):
+
+    plt.figure(figsize = (10,6))
+
+    for s in strats:
+
+        label = f"{res['strategy']}"
+
+        plt.plot(res["lapTimes"], label = label)
+
+    plt.xlabel("Lap")
+    plt.ylabel("Lap Time (secs)")
+    plt.title("Race Strategy Simulation")
+    plt.legend()
+    plt.show()
