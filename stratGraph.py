9710ca41896bfef55b77570f5336f748900434a3
nathansizer <52630545+nathansizer@users.noreply.github.com>
2025-09-22 18:33:01 +0100

    Update stratGraph to show cumulative race time, not lap time per lap

--- stratGraph.py
+++ stratGraph.py
@@ -2,6 +2,7 @@
 Strategy Graph and plotting class
 """
 import matplotlib.pyplot as plt
+import numpy as np
 
 #plot the strategies on a graph
 def plotStrategies(strats):
@@ -12,7 +13,8 @@ def plotStrategies(strats):
 
         label = f"{s['strategy']}"
 
-        plt.plot(s["lapTimes"], label = label)
+        totalTime = np.cumsum(s["lapTimes"])
+        plt.plot(range(1, len(totalTime) + 1), totalTime, label = label)
 
     plt.xlabel("Lap")
     plt.ylabel("Lap Time (secs)")
